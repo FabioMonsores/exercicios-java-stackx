606999016567fefbdc129cf9ce07a5219de4100f
Fábio Monsores <fabio2009pbi@gmail.com>
2022-05-07 14:36:25 -0300

    Exercícios de JAVA para treino da StackX

---
+++ src/exerciciosRepeticaoEVetores/Exercicio03.java
@@ -0,0 +1,24 @@
+/*Escreva um programa que peça para o usuário informar qual tabuada 
+ * o mesmo deseja ver e armazene o número em uma variável.
+ *  Em seguida, imprima a tabuada na tela se utilizando da instrução FOR.*/
+
+package exerciciosRepeticaoEVetores;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite o número de qual tabuada deseja ver: ");
+		Integer num = ler.nextInt();
+		ler.close();
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num + "x" + i + " = " + (i * num));
+
+		}
+
+	}
+}
